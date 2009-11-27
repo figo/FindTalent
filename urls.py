d3cd27bbc0667d722f42d4dbfc7643eae2cbaabe
Tian You <axqd001@gmail.com>
2009-11-27 19:39:08 +0800

    add error page

--- urls.py
+++ urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
   # Uncomment the next line to enable the admin:
   # ( r'^admin/', include( admin.site.urls ) ),
   ( r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root' : settings.STATIC_DOC_ROOT } ),
+  ( r'^error/$', 'common.views.error' ),
 )
 
 urlpatterns += patterns( 'account',
