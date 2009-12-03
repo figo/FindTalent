4668572f6378157704c8eb1728af89e9e446d0c8
Tian You <axqd001@gmail.com>
2009-12-03 17:00:03 +0800

    extract show data from show model

--- show/views.py
+++ show/views.py
@@ -2,12 +2,18 @@
 from django.shortcuts import render_to_response
 from django.http      import HttpResponseRedirect
 
-from FindTalent.common.models import SessionManager
+from FindTalent.account.models import TalentUser
+from FindTalent.common.models  import SessionManager
 
 def show( request ):
   ident = SessionManager( request.session ).check_cookie()
 
   if len( ident ) == 2:
-    return render_to_response( 'show/index.htm', { 'username' : ident[ 0 ] } )
+    user = TalentUser.objects.filter( username__exact = ident[ 0 ] )[:1].values()
+    if len( user ) == 1:
+      user = user[ 0 ]
+      return render_to_response( 'show/index.htm', { 'username' : user['username'], 'title' : user['title'], 'team' : user['team'] } )
+    else:
+      return HttpResponseRedirect( '/error/?msg=Invalid cookie' )
   else:
     return HttpResponseRedirect( '/' )
