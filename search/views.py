5b0d62e151f26d397397d8d843090348205a2333
Figo Luo <luohui925@gmail.com>
2009-12-15 23:28:32 +0800

    update search() function with actual search happen

--- search/views.py
+++ search/views.py
@@ -1,13 +1,32 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http      import HttpResponseRedirect
-
-from FindTalent.common.models import SessionManager
+from FindTalent.common.models import TalentHelper,SessionManager
+from FindTalent.user_profile.skill.models import Skill
+from FindTalent.user_profile.project.models import Project
+from FindTalent.user_profile.certification.models import Certification
+from FindTalent.account.models import TalentUser
 
 def search( request ):
   ident = SessionManager( request.session ).check_cookie()
 
-  if len( ident ) == 2:
-    return render_to_response( 'search/index.htm', { 'username' : ident[ 0 ] } )
-  else:
+  if len( ident ) != 2:
     return HttpResponseRedirect( '/' )
+ 
+  checker = TalentHelper.get_empty_val_checker( request.GET )
+  if not checker( 'type' ) or not checker ( 'key' ):
+       result = Project.objects.all()
+       return render_to_response( 'search/index.htm', { 'all_search_result' : result, 'total_qualified':len(result) } )
+  
+  else:
+       search_type = request.GET[ 'type' ]
+       search_key  = request.GET[ 'key' ]
+       
+       if ( search_type == 'project' ):
+         result = Project.objects.filter( description__icontains=search_key )
+       if ( search_type == 'skill' ):
+         result = Skill.objects.filter( skill__icontains=search_key )
+       if ( search_type == 'certification' ):
+         result = Certification.objects.filter( cert_name__icontains=search_key )
+       
+       return render_to_response( 'search/index.htm', { 'all_search_result' : result, 'total_qualified':len(result) } )
